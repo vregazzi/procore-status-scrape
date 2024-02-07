28fce086331155fecb8aba4587b7352f7228cc9b
Vincent Regazzi <vincentregazzi@gmail.com>
2024-02-07 15:22:45 -0600

    set log level

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from procore import check_status as procore_check_status
 from procore import scrape_page as procore_scrape_page
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     operational = procore_check_status()
+
     if operational:
         logging.info("All systems operational")
     else:
