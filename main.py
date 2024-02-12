9b853781af11f956d416ca603b6013628e7a1ef0
Vincent Regazzi <vincentregazzi@gmail.com>
2024-02-12 12:49:01 -0600

    remove whitespace

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def get_info():
         logging.info("All systems operational")
         _upload(time)
         return
-    
+
     logging.warning("Some systems are down")
     logging.info("Scraping page for more information")
 
