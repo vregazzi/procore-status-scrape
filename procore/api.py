ff9c50b80068fb4a51df5615c05cbcce95ee085a
Vincent Regazzi <vincentregazzi@gmail.com>
2024-02-15 15:40:30 -0600

    linting

--- procore/api.py
+++ procore/api.py
@@ -5,8 +5,9 @@ def check_status() -> bool:
     response = requests.get("https://status.procore.com/api/v2/status.json")
     data = response.json()["status"]
 
-    if (data["description"] == "All Systems Operational" and 
-        data["indicator"]) == "none":
+    if (
+        data["description"] == "All Systems Operational" and data["indicator"]
+    ) == "none":
         return True
 
     return False
