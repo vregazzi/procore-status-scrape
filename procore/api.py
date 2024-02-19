1c65e9cdd0b4086291d312463849e47af25f96f7
Vincent Regazzi <vincentregazzi@gmail.com>
2024-02-19 11:18:57 -0600

    fix line break up

--- procore/api.py
+++ procore/api.py
@@ -5,9 +5,8 @@ def check_status() -> bool:
     response = requests.get("https://status.procore.com/api/v2/status.json")
     data = response.json()["status"]
 
-    if (
-        data["description"] == "All Systems Operational" and data["indicator"]
-    ) == "none":
+    if (data["description"] == "All Systems Operational"
+            and data["indicator"]) == "none":
         return True
 
     return False
